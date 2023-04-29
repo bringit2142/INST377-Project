0cc9512fe41949764365e758e358f1052d68d2c0
Caleb Andree <candree@terpmail.umd.edu>
2023-04-29 16:36:01 -0400

    Filter and reset button now work on the graph

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
-async function fetchUniversities() {
-  const url =
-    "http://universities.hipolabs.com/search";
+// Declare a variable to store the chart object
+let universitiesChart = null;
+
+// Add event listener to checkbox and input elements
+const topCountriesButton = document.getElementById("top-countries-button");
+const topCountriesInput = document.getElementById("top-countries-input");
+const resetButton = document.getElementById("reset-button");
+
+// Call fetchUniversities() function on page load
+fetchUniversities();
+
+// Add event listeners to load and reset buttons
+topCountriesButton.addEventListener("click", handleClick);
+resetButton.addEventListener("click", handleReset);
+
+function handleClick() {
+  const inputValue = topCountriesInput.value.trim();
+  if (inputValue !== '') {
+    fetchUniversities(inputValue);
+  } else {
+    fetchUniversities();
+  }
+}
+
+function handleReset() {
+  fetchUniversities();
+}
+
+async function fetchUniversities(topCountries) {
+  const url = "http://universities.hipolabs.com/search";
 
   try {
     const response = await fetch(url);
     const universities = await response.json();
-    const universitiesData = universities.reduce((acc, curr) => {
+    let universitiesData = universities.reduce((acc, curr) => {
       const country = curr.country;
       acc[country] = acc[country] || 0;
       acc[country]++;
       return acc;
     }, {});
 
-    console.log(universitiesData);
+    if (topCountries) {
+      const sortedCountries = Object.keys(universitiesData).sort(
+        (a, b) => universitiesData[b] - universitiesData[a]
+      );
+      const topCountriesData = {};
+      for (let i = 0; i < topCountries; i++) {
+        const country = sortedCountries[i];
+        topCountriesData[country] = universitiesData[country];
+      }
+      universitiesData = topCountriesData;
+    }
+
+    // Destroy the previous chart before creating a new one
+    if (universitiesChart !== null) {
+      universitiesChart.destroy();
+    }
+
     const ctx = document.getElementById("universitiesChart").getContext("2d");
-    const chart = new Chart(ctx, {
+    universitiesChart = new Chart(ctx, {
       type: "bar",
       data: {
         labels: Object.keys(universitiesData),
@@ -33,7 +76,7 @@ async function fetchUniversities() {
           xAxes: [
             {
               ticks: {
-                barPercentage: 0.9, 
+                barPercentage: 0.9,
               },
             },
           ],
@@ -51,5 +94,3 @@ async function fetchUniversities() {
     console.error(error);
   }
 }
-
-fetchUniversities();
