78d73c239dfa11080f17df9102b61cf8359322fd
Caleb Andree <candree@terpmail.umd.edu>
2023-05-04 00:36:31 -0400

    Adding "about" description, localStorage edit also

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const topCountriesButton = document.getElementById("top-countries-button");
 const topCountriesInput = document.getElementById("top-countries-input");
 const resetButton = document.getElementById("reset-button");
 
-// Call fetchUniversities() function on page load
-fetchUniversities();
+// Call loadUniversities() function on page load
+loadUniversities();
 
 // Add event listeners to load and reset buttons
 topCountriesButton.addEventListener("click", handleClick);
@@ -16,14 +16,47 @@ resetButton.addEventListener("click", handleReset);
 function handleClick() {
   const inputValue = topCountriesInput.value.trim();
   if (inputValue !== '') {
-    fetchUniversities(inputValue);
+    const universitiesData = JSON.parse(localStorage.getItem('universitiesData'));
+    if (universitiesData) {
+      const filteredData = filterUniversities(universitiesData, inputValue);
+      createChart(filteredData);
+    } else {
+      fetchUniversities(inputValue);
+    }
+  } else {
+    const universitiesData = JSON.parse(localStorage.getItem('universitiesData'));
+    if (universitiesData) {
+      createChart(universitiesData);
+    } else {
+      fetchUniversities();
+    }
+  }
+}
+
+function handleReset() {
+  localStorage.removeItem('universitiesData');
+  loadUniversities();
+}
+
+function loadUniversities() {
+  const universitiesData = JSON.parse(localStorage.getItem('universitiesData'));
+  if (universitiesData) {
+    createChart(universitiesData);
   } else {
     fetchUniversities();
   }
 }
 
-function handleReset() {
-  fetchUniversities();
+function filterUniversities(universitiesData, inputValue) {
+  const sortedCountries = Object.keys(universitiesData).sort((a, b) => universitiesData[b] - universitiesData[a]);
+  const filteredData = {};
+  for (let i = 0; i < inputValue; i++) {
+    const country = sortedCountries[i];
+    if (country) {
+      filteredData[country] = universitiesData[country];
+    }
+  }
+  return filteredData;
 }
 
 async function fetchUniversities(topCountries) {
@@ -40,57 +73,55 @@ async function fetchUniversities(topCountries) {
     }, {});
 
     if (topCountries) {
-      const sortedCountries = Object.keys(universitiesData).sort(
-        (a, b) => universitiesData[b] - universitiesData[a]
-      );
-      const topCountriesData = {};
-      for (let i = 0; i < topCountries; i++) {
-        const country = sortedCountries[i];
-        topCountriesData[country] = universitiesData[country];
-      }
-      universitiesData = topCountriesData;
+      universitiesData = filterUniversities(universitiesData, topCountries);
     }
 
-    // Destroy the previous chart before creating a new one
-    if (universitiesChart !== null) {
-      universitiesChart.destroy();
-    }
-
-    const ctx = document.getElementById("universitiesChart").getContext("2d");
-    universitiesChart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: Object.keys(universitiesData),
-        datasets: [
-          {
-            label: "Number of Universities",
-            data: Object.values(universitiesData),
-            backgroundColor: "rgba(75, 192, 192, 0.2)",
-            borderColor: "rgba(75, 192, 192, 1)",
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          xAxes: [
-            {
-              ticks: {
-                barPercentage: 0.9,
-              },
-            },
-          ],
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-      },
-    });
+    localStorage.setItem('universitiesData', JSON.stringify(universitiesData));
+    console.log(localStorage);
+    createChart(universitiesData);
   } catch (error) {
     console.error(error);
   }
+}
+
+function createChart(universitiesData) {
+  // Destroy the previous chart before creating a new one
+  if (universitiesChart !== null) {
+    universitiesChart.destroy();
+  }
+
+  const ctx = document.getElementById("universitiesChart").getContext("2d");
+  universitiesChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: Object.keys(universitiesData),
+      datasets: [
+        {
+          label: "Number of Universities",
+          data: Object.values(universitiesData),
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              barPercentage: 0.9,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
 }
