672da76b174857841a90fdb1de959714f97e85f1
bringit2142 <102170516+bringit2142@users.noreply.github.com>
2023-05-08 22:02:56 -0400

    Adding comments to JS files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Declare a variable to store the chart object
+/// Declare a variable to store the chart object
 let universitiesChart = null;
 
 // Add event listener to checkbox and input elements
@@ -13,58 +13,90 @@ loadUniversities();
 topCountriesButton.addEventListener("click", handleClick);
 resetButton.addEventListener("click", handleReset);
 
+// Function to handle click event on the "Show Top Universities" button
 function handleClick() {
+  // Get the input value and remove any leading/trailing white space
   const inputValue = topCountriesInput.value.trim();
-  if (inputValue !== '') {
-    const universitiesData = JSON.parse(localStorage.getItem('universitiesData'));
+
+  // Check if input value is not empty
+  if (inputValue !== "") {
+    // Try to get universities data from localStorage
+    const universitiesData = JSON.parse(
+      localStorage.getItem("universitiesData")
+    );
     if (universitiesData) {
+      // Filter the universities data based on the input value
       const filteredData = filterUniversities(universitiesData, inputValue);
+      // Create the chart using the filtered data
       createChart(filteredData);
     } else {
+      // If universities data not found in localStorage, fetch it from the API
       fetchUniversities(inputValue);
     }
   } else {
-    const universitiesData = JSON.parse(localStorage.getItem('universitiesData'));
+    // If input value is empty, show all universities
+    const universitiesData = JSON.parse(
+      localStorage.getItem("universitiesData")
+    );
     if (universitiesData) {
+      // Create the chart using all universities data
       createChart(universitiesData);
     } else {
+      // If universities data not found in localStorage, fetch it from the API
       fetchUniversities();
     }
   }
 }
 
+// Function to handle click event on the "Reset" button
 function handleReset() {
-  localStorage.removeItem('universitiesData');
+  // Remove universities data from localStorage
+  localStorage.removeItem("universitiesData");
+  // Load universities data from API and create chart
   loadUniversities();
 }
 
+// Function to load universities data
 function loadUniversities() {
-  const universitiesData = JSON.parse(localStorage.getItem('universitiesData'));
+  // Try to get universities data from localStorage
+  const universitiesData = JSON.parse(localStorage.getItem("universitiesData"));
   if (universitiesData) {
+    // If universities data found in localStorage, create chart using it
     createChart(universitiesData);
   } else {
+    // If universities data not found in localStorage, fetch it from the API
     fetchUniversities();
   }
 }
 
+// This function filters universities data based on the number of top countries requested
 function filterUniversities(universitiesData, inputValue) {
-  const sortedCountries = Object.keys(universitiesData).sort((a, b) => universitiesData[b] - universitiesData[a]);
+  // Sorts the countries in descending order based on the number of universities
+  const sortedCountries = Object.keys(universitiesData).sort(
+    (a, b) => universitiesData[b] - universitiesData[a]
+  );
+  // Creates a new object to store the filtered data
   const filteredData = {};
+  // Iterates through the sorted list of countries and adds data to the filtered data object up to the input value
   for (let i = 0; i < inputValue; i++) {
     const country = sortedCountries[i];
     if (country) {
       filteredData[country] = universitiesData[country];
     }
   }
+  // Returns the filtered data object
   return filteredData;
 }
 
+// Asynchronous function to fetch data from API
 async function fetchUniversities(topCountries) {
   const url = "http://universities.hipolabs.com/search";
 
   try {
     const response = await fetch(url);
     const universities = await response.json();
+
+    // Reduce array of universities to an object with countries as keys and counts of universities as values
     let universitiesData = universities.reduce((acc, curr) => {
       const country = curr.country;
       acc[country] = acc[country] || 0;
@@ -72,24 +104,30 @@ async function fetchUniversities(topCountries) {
       return acc;
     }, {});
 
+    // If topCountries is specified, filter the universitiesData to only include the top countries
     if (topCountries) {
       universitiesData = filterUniversities(universitiesData, topCountries);
     }
 
-    localStorage.setItem('universitiesData', JSON.stringify(universitiesData));
+    // Store the universitiesData in local storage as a JSON string
+    localStorage.setItem("universitiesData", JSON.stringify(universitiesData));
     console.log(localStorage);
+
+    // Create a chart using the universitiesData
     createChart(universitiesData);
   } catch (error) {
     console.error(error);
   }
 }
 
+// Function to create a chart using the given universitiesData object
 function createChart(universitiesData) {
   // Destroy the previous chart before creating a new one
   if (universitiesChart !== null) {
     universitiesChart.destroy();
   }
 
+  // Get the canvas element and create a new Chart object using the universitiesData
   const ctx = document.getElementById("universitiesChart").getContext("2d");
   universitiesChart = new Chart(ctx, {
     type: "bar",
--- university_table.js
+++ university_table.js
@@ -1,18 +1,25 @@
+// Get reference to country select element and add change event listener
 const countrySelect = document.getElementById("country-select");
 countrySelect.addEventListener("change", handleCountrySelect);
 
+// Function to handle country select change event
 async function handleCountrySelect(event) {
+  // Get the selected country value
   const selectedCountry = event.target.value;
+  // If selected country is empty, clear the table and return
   if (selectedCountry === "") {
     clearTable();
     return;
   }
 
+  // Declare variable to hold universities data
   let universities;
+  // Check if universities data exists in localStorage, if yes, parse it
   const localStorageData = localStorage.getItem("universities");
   if (localStorageData) {
     universities = JSON.parse(localStorageData);
   } else {
+    // If no, fetch universities data from the API and set it in localStorage
     const url = "http://universities.hipolabs.com/search";
     try {
       const response = await fetch(url);
@@ -24,6 +31,7 @@ async function handleCountrySelect(event) {
     }
   }
 
+  // Filter universities by selected country and update the table with filtered data
   const filteredUniversities = universities.filter(
     (university) =>
       university.country &&
@@ -32,13 +40,17 @@ async function handleCountrySelect(event) {
   updateTable(filteredUniversities);
 }
 
+// This function populates a dropdown menu with a list of countries that have universities, using data from an external API and the browser's local storage
 async function populateCountries() {
   const url = "http://universities.hipolabs.com/search";
   let universities;
+
+  // Check if data is already stored in local storage
   const localStorageData = localStorage.getItem("universities");
   if (localStorageData) {
     universities = JSON.parse(localStorageData);
   } else {
+    // Fetch data from external API if not in local storage and save it in local storage for future use
     try {
       const response = await fetch(url);
       universities = await response.json();
@@ -49,6 +61,7 @@ async function populateCountries() {
     }
   }
 
+  // Create a Set of unique country names from the universities data
   const countries = new Set();
   universities.forEach((university) => {
     if (university.country) {
@@ -56,6 +69,7 @@ async function populateCountries() {
     }
   });
 
+  // Populate the dropdown menu with options for each country
   const countrySelect = document.getElementById("country-select");
   for (let country of countries) {
     const option = document.createElement("option");
@@ -65,35 +79,43 @@ async function populateCountries() {
   }
 }
 
+// This function clears the content of the table body.
 function clearTable() {
   const tbody = document.querySelector("#university-table tbody");
   tbody.innerHTML = "";
 }
 
+// This function updates the content of the table with the provided universities.
 function updateTable(universities) {
   const tbody = document.querySelector("#university-table tbody");
   tbody.innerHTML = "";
 
+  // Log the universities being added to the table.
   console.log("Updating table with universities:", universities);
 
   universities.forEach((university) => {
+    // Log the university being added to the table.
     console.log("Adding row for university:", university);
 
+    // Create new table row and table data cells for name and web page.
     const row = document.createElement("tr");
     const nameCell = document.createElement("td");
     const webPageCell = document.createElement("td");
 
+    // Log the name and webpage of the university.
     console.log("University name:", university.name);
     console.log("University webpage:", university.web_pages[0]);
 
+    // Add the university's name and webpage to the corresponding cells.
     nameCell.textContent = university.name || "-";
     webPageCell.textContent = university.web_pages[0] || "-";
 
+    // Append the cells to the row and the row to the table body.
     row.appendChild(nameCell);
     row.appendChild(webPageCell);
-
     tbody.appendChild(row);
   });
 }
 
+// Call populateCountries function to initialize the page.
 populateCountries();
